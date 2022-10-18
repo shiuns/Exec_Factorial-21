0476b54abec507f55aa7d0d7ac3ce8bbaab68cde
shiuns <shiunworks@gmail.com>
2022-10-19 02:23:06 +0800

    加入專案檔案。

---
+++ [作業(Exec_Factorial)]-21謝孟勳/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_Factorial___21謝孟勳
+{
+    //計算數值的階層
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("您要的階乘數字是: ");
+            string input = Console.ReadLine();
+
+            int sum = 1;
+            for (int i = 1; i <= Convert.ToInt32(input); i++)
+            {
+                sum *= i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($" {input} 的階層乘積是 {sum} ");
+        }
+    }
+}
